a83f3571b0c333b45d224a9bd3060a21362c8196
Ethan Favia <efavia215@live.com>
2017-03-15 16:01:59 -0400

    work on user schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,23 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
+var schema = mongoose.Schema; 
+// the user schema attributes / characteristics / and fields 
 
+var UserSchema = new mongoose.Schema({
+    email: {type: String, unique: true, lowercase: true},
+    password: String, 
+
+
+    profile: {
+        name: {type: String, deafault: ''},
+        picture: {type: String, default: ''}
+    },
+
+    address: String,
+    history: [{
+        date: Date,
+        paid: {type: Number, default: 0},
+
+    }]
+    
+})
