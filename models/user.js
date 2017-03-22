86f651d5816517ac3604bd0370801d3bafc59a09
Ethan Favia <efavia215@live.com>
2017-03-22 15:30:31 -0400

    work on the user login

--- models/user.js
+++ models/user.js
@@ -21,3 +21,17 @@ var UserSchema = new mongoose.Schema({
     }]
     
 })
+
+UserSchema.pre('save', function(next){
+    var user = this;
+    if(!user.isModified('password')) return next();
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+            if (err) return(next(err))
+
+            user.password = hash;
+        })
+    });
+})
