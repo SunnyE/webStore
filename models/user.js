5993e068bc75632a3e548635d506c27ee93cfe93
Ethan Favia <efavia215@live.com>
2017-04-04 18:06:59 -0400

    more work on user model fixing database error

--- models/user.js
+++ models/user.js
@@ -45,4 +45,4 @@ UserSchema.methods.comparePassword = function (password) {
     return bcrypt.compareSync(password, this.password)
 };
 
-modue.exports = mongoose.model('User', UserSchema); 
+module.exports = mongoose.model('User', UserSchema); 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser'); 
+
+var User = require('./models/user');
 
 var app = express();
 
@@ -10,14 +13,30 @@ mongoose.connect('mongodb://root:12345@ds147520.mlab.com:47520/ecommerce', funct
     } else {
         console.log("connected to database");
     }
-})
-// middle war 
+});
+// middle ware 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res) {
     var name = 'Ethan';
     res.json("My name is " + name); 
-})
+});
+
+app.post('/create-user', function(req,res, next) {
+    var user = new User();
+
+    user.profile.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email; 
+
+    user.save(function(err){
+        if (err) next(err);
+
+        res.json('Successfully created new user'); 
+    })
+});
 
 app.listen(3000, function(err){
     if (err) throw err;
